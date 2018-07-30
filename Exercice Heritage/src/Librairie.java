161d57b9d8fd13981f86936fe84574c9f0dd236f
ATournel <a.tournel@hotmail.fr>
2018-07-30 11:59:39 +0200

    Crea Project and main Class

---
+++ Exercice Heritage/src/Librairie.java
@@ -0,0 +1,13 @@
+
+public class Librairie {
+
+	String nom= "Mibrairie l'Heritage";
+	String adresse= "28 rue de la page";
+	String horaires= "Du Lundi au Vendredi de 9h30 à 17h30";
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
