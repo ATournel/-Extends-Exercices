f03c1d0ac7d58de0626e37089e7fac0f9360e6cb
ATournel <a.tournel@hotmail.fr>
2018-07-30 12:02:02 +0200

    Crea master class Produits

---
+++ Exercice Heritage/src/Produits.java
@@ -0,0 +1,42 @@
+
+public class Produits {
+
+	String titre;
+	String auteur;
+	String editeur;
+	double prix;
+	boolean disponible;
+	
+	
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getAuteur() {
+		return auteur;
+	}
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+	public String getEditeur() {
+		return editeur;
+	}
+	public void setEditeur(String editeur) {
+		this.editeur = editeur;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public boolean isDisponible() {
+		return disponible;
+	}
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+	
+}
